675dec5a36d16311cd11156fff3ab51eb7cae00e
wangqiaoling <wangqiaoling@baidu.com>
2021-10-25 15:20:36 +0800

    添加mocker和demo

--- reskript.config.js
+++ reskript.config.js
@@ -13,6 +13,6 @@ exports.build = {
 exports.devServer = {
     port: 8788,
     // TODO: 修改后端API代理的配置
-    apiPrefixes: ['/hotspot/api', '/hotspot/browse'],
-    defaultProxyDomain: '', // TODO: 设置后端测试环境域名
+    apiPrefixes: ['/api'],
+    defaultProxyDomain: '127.0.0.1:3722', // TODO: 设置后端测试环境域名
 };
